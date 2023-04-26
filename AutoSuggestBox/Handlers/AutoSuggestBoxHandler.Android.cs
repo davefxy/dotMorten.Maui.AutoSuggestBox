6e6eb8be3d97561f97ba8c94fc80801def45ab5f
Davefxy <davefxy@yahoo.com>
2023-04-26 07:25:07 -0600

    Removed unused module Modify AutoSuggestBox project file for generating output files

--- AutoSuggestBox/Handlers/AutoSuggestBoxHandler.Android.cs
+++ AutoSuggestBox/Handlers/AutoSuggestBoxHandler.Android.cs
@@ -89,56 +89,29 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
     private void UpdateTextColor(AutoSuggestBoxView platformView)
     {
         var color = VirtualView?.TextColor;
-#if NETFX_CORE
-            Control.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb((byte)(color.A * 255), (byte)(color.R * 255), (byte)(color.G * 255), (byte)(color.B * 255)));
-#elif __ANDROID__ || __IOS__
         platformView.SetTextColor(color.ToPlatform());
-#endif
     }
     private void UpdatePlaceholderTextColor(AutoSuggestBoxView platformView)
     {
         var placeholderColor = VirtualView?.PlaceholderTextColor;
-#if NETFX_CORE
-            // Not currently supported by UWP's control
-            // UpdateColor(placeholderColor, ref _placeholderDefaultBrush,
-            //     () => Control.PlaceholderForegroundBrush, brush => Control.PlaceholderForegroundBrush = brush);
-            // UpdateColor(placeholderColor, ref _defaultPlaceholderColorFocusBrush,
-            //     () => Control.PlaceholderForegroundFocusBrush, brush => Control.PlaceholderForegroundFocusBrush = brush);
-#elif __ANDROID__ || __IOS__
         platformView.SetPlaceholderTextColor(placeholderColor);
-#endif
     }
 
     private void UpdateDisplayMemberPath(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
-#if NETFX_CORE
-            Control.DisplayMemberPath = Element.DisplayMemberPath;
-#elif __ANDROID__ || __IOS__
         handler.PlatformView.SetItems(view ?.ItemsSource?.OfType<object>(), (o) => FormatType(o, view?.DisplayMemberPath), (o) => FormatType(o, view?.TextMemberPath));
-#endif
     }
 
     private void UpdateIsEnabled(AutoSuggestBoxView platformView)
     {
-#if NETFX_CORE
-            platformView.IsEnabled = Element.IsEnabled;
-#elif __ANDROID__
         platformView.Enabled = (bool)(VirtualView?.IsEnabled);
-#elif __IOS__
-            platformView.UserInteractionEnabled = Element.IsEnabled;
-#endif
     }
 
     private void UpdateItemsSource(AutoSuggestBoxView platformView)
     {
-#if NETFX_CORE
-            platformView.ItemsSource = Element?.ItemsSource;
-#elif __ANDROID__ || __IOS__
         platformView.SetItems(VirtualView?.ItemsSource?.OfType<object>(), (o) => FormatType(o, VirtualView?.DisplayMemberPath), (o) => FormatType(o, VirtualView?.TextMemberPath));
-#endif
     }
 
-#if __ANDROID__ || __IOS__
     private static string FormatType(object instance, string memberPath)
     {
         if (!string.IsNullOrEmpty(memberPath))
@@ -146,6 +119,4 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
         else
             return instance?.ToString() ?? "";
     }
-#endif
-
 }
--- AutoSuggestBox/Handlers/AutoSuggestBoxHandler.iOS.cs
+++ AutoSuggestBox/Handlers/AutoSuggestBoxHandler.iOS.cs
@@ -1,6 +1,9 @@
 ﻿#nullable enable
 using Microsoft.Maui.Handlers;
 using Maui.AutoSuggestBox.Platforms.iOS;
+using Microsoft.Maui.Platform;
+using System.Drawing;
+using UIKit;
 
 namespace Maui.AutoSuggestBox.Handlers;
 
@@ -12,12 +15,65 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
     protected override void ConnectHandler(AutoSuggestBoxView platformView)
     {
         base.ConnectHandler(platformView);
+
+        //UpdateTextColor(platformView);
+        //UpdatePlaceholderText(platformView);
+        //UpdatePlaceholderTextColor(platformView);
+        //UpdateIsEnabled(platformView);
+
+        platformView.SuggestionChosen += OnPlatformViewSuggestionChosen;
+        platformView.TextChanged += OnPlatformViewTextChanged;
+        platformView.QuerySubmitted += OnPlatformViewQuerySubmitted;
+        //platformView.UpdateTextOnSelect = e.NewElement.UpdateTextOnSelect;
+        //platformView.IsSuggestionListOpen = e.NewElement.IsSuggestionListOpen;
+        //Control.EditingDidBegin += Control_EditingDidBegin;
+        //Control.EditingDidEnd += Control_EditingDidEnd;
+        //Frame = new RectangleF(0, 20, 320, 40);
     }
     protected override void DisconnectHandler(AutoSuggestBoxView platformView)
     {
+        platformView.SuggestionChosen -= OnPlatformViewSuggestionChosen;
+        platformView.TextChanged -= OnPlatformViewTextChanged;
+        platformView.QuerySubmitted -= OnPlatformViewQuerySubmitted;
+
         platformView.Dispose();
         base.DisconnectHandler(platformView);
     }
+
+    private void OnPlatformViewSuggestionChosen(object? sender, AutoSuggestBoxSuggestionChosenEventArgs e)
+    {
+        VirtualView?.RaiseSuggestionChosen(e);
+    }
+    private void OnPlatformViewTextChanged(object? sender, AutoSuggestBoxTextChangedEventArgs e)
+    {
+        VirtualView?.NativeControlTextChanged(e);
+    }
+    private void OnPlatformViewQuerySubmitted(object? sender, AutoSuggestBoxQuerySubmittedEventArgs e)
+    {
+        VirtualView?.RaiseQuerySubmitted(e);
+    }
+
+    static readonly int baseHeight = 10;
+    /// <inheritdoc />
+    //public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+    //{
+    //    var baseResult = base.GetDesiredSize(widthConstraint, heightConstraint);
+    //    var testString = new Foundation.NSString("Tj");
+    //    var testSize = testString.GetSizeUsingAttributes(new UIStringAttributes { Font = Control.Font });
+    //    double height = baseHeight + testSize.Height;
+    //    height = Math.Round(height);
+
+    //    return new SizeRequest(new global::Xamarin.Forms.Size(baseResult.Request.Width, height));
+    //}
+
+    //private void Control_EditingDidBegin(object sender, EventArgs e)
+    //{
+    //    Element?.SetValue(VisualElement.IsFocusedPropertyKey, true);
+    //}
+    //private void Control_EditingDidEnd(object sender, EventArgs e)
+    //{
+    //    Element?.SetValue(VisualElement.IsFocusedPropertyKey, false);
+    //}
 
     public static void MapText(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
@@ -31,6 +87,7 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
     }
     public static void MapPlaceholderText(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
+        //handler.PlatformView.Hint = view.PlaceholderText;
         handler.PlatformView.PlaceholderText = view.PlaceholderText;
     }
     public static void MapPlaceholderTextColor(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
@@ -39,11 +96,11 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
     }
     public static void MapTextMemberPath(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
-
+        handler.PlatformView?.SetItems(view.ItemsSource?.OfType<object>(), (o) => FormatType(o, view.DisplayMemberPath), (o) => FormatType(o, view.TextMemberPath));
     }
     public static void MapDisplayMemberPath(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
-
+        handler.PlatformView.SetItems(view?.ItemsSource?.OfType<object>(), (o) => FormatType(o, view?.DisplayMemberPath), (o) => FormatType(o, view?.TextMemberPath));
     }
     public static void MapIsSuggestionListOpen(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
@@ -51,15 +108,37 @@ public partial class AutoSuggestBoxHandler : ViewHandler<IAutoSuggestBox, AutoSu
     }
     public static void MapUpdateTextOnSelect(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
-
+        handler.PlatformView.UpdateTextOnSelect = view.UpdateTextOnSelect;
     }
     public static void MapIsEnabled(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
-
+        handler.PlatformView.UserInteractionEnabled = view.IsEnabled;
     }
     public static void MapItemsSource(AutoSuggestBoxHandler handler, IAutoSuggestBox view)
     {
         handler.PlatformView.SetItems(view?.ItemsSource?.OfType<object>(), (o) => FormatType(o, view?.DisplayMemberPath), (o) => FormatType(o, view?.TextMemberPath));
+    }
+
+    private void UpdateTextColor(AutoSuggestBoxView platformView)
+    {
+        var color = VirtualView?.TextColor;
+        platformView.SetTextColor(color);
+    }
+    private void UpdatePlaceholderTextColor(AutoSuggestBoxView platformView)
+    {
+        var placeholderColor = VirtualView?.PlaceholderTextColor;
+        platformView.SetPlaceholderTextColor(placeholderColor);
+    }
+    private void UpdatePlaceholderText(AutoSuggestBoxView platformView) => platformView.PlaceholderText = VirtualView?.PlaceholderText;
+
+    private void UpdateIsEnabled(AutoSuggestBoxView platformView)
+    {
+        platformView.UserInteractionEnabled = (bool)(VirtualView?.IsEnabled);
+    }
+
+    private void UpdateItemsSource(AutoSuggestBoxView platformView)
+    {
+        platformView.SetItems(VirtualView?.ItemsSource?.OfType<object>(), (o) => FormatType(o, VirtualView?.DisplayMemberPath), (o) => FormatType(o, VirtualView?.TextMemberPath));
     }
     private static string FormatType(object instance, string memberPath)
     {
